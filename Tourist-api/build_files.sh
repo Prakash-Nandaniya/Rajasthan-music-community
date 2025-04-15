d60a29c58212a756da587eca569bf26fec9827d2
Prakash-Nandaniya <prakashnandaniya12@gmail.com>
2025-04-15 23:05:37 +0530

    removed static files part

--- Tourist-api/build_files.sh
+++ Tourist-api/build_files.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
-python3.9 manage.py migrate
+python3.9 -m pip install -r requirements.txt || echo "Pip install failed"
+python3.9 manage.py migrate || echo "Migrate failed"
 echo "BUILD END"
